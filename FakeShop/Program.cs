7bb3853396344dfaf08d699cda634ce8c084d2ec
whyme0 <sparkjetstudiost@gmail.com>
2022-09-26 22:52:45 +0300

    Add Serilog to project

--- FakeShop/Program.cs
+++ FakeShop/Program.cs
@@ -1,10 +1,23 @@
+using Serilog;
+using Serilog.Events;
 using FakeShop.Data;
 using FakeShop.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+string rootPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
 var builder = WebApplication.CreateBuilder(args);
 string connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        Path.Combine(rootPath, "Logging\\log-.txt"),
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+        rollingInterval: RollingInterval.Day,
+        restrictedToMinimumLevel: LogEventLevel.Information)
+    .CreateLogger();
+builder.Host.UseSerilog();
 
 builder.Services.AddDbContextPool<ShopDbContext>(b =>
     b.UseMySql(connString, ServerVersion.AutoDetect(connString)));
