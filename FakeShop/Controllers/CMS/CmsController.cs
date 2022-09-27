0e5af83cf67dd09ce8494e5598b96ca6eb8e6125
whyme0 <sparkjetstudiost@gmail.com>
2022-09-27 21:58:49 +0300

    init cms controller

---
+++ FakeShop/Controllers/CMS/CmsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FakeShop.Controllers
+{
+    public partial class CmsController : Controller
+    {
+        private readonly ILogger _logger;
+        
+        public CmsController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("cms")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
