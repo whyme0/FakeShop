035f2fd0a7d032cfcc8650e8c04dddf1289a6e07
whyme0 <sparkjetstudiost@gmail.com>
2022-09-27 22:00:08 +0300

    adds products control to cmscontroller

---
+++ FakeShop/Controllers/CMS/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FakeShop.Models;
+
+namespace FakeShop.Controllers.CMS
+{
+    [Route("[controller]/products/")]
+    public partial class CmsController : Controller
+    {
+
+        [Route("")]
+        public IActionResult List()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("edit")]
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("edit/{id:int}")]
+        [HttpPost]
+        public IActionResult Edit(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("delete/{id:int}")]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
