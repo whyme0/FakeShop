268ba98847ffe73b600cefd85edfdde3f86784ba
whyme0 <sparkjetstudiost@gmail.com>
2022-10-04 10:00:19 +0300

    create custom, required attribute

---
+++ FakeShop/Attributes/RequiredAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace FakeShop.Attributes
+{
+    public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute
+    {
+        public RequiredAttribute()
+        {
+            ErrorMessage = "This field is required";
+        }
+    }
+}
